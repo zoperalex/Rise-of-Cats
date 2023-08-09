d67d92b7a3ebf073de056b31c1ecd190f66e57dd
zoperalex <alex.michaelides17@gmail.com>
2023-08-10 02:20:57 +0300

    Started work on player projectile attacks.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject enemies;
     public Camera mainCamera;
     public float speed = 3;
     public FloatingJoystick floatingJoystick;
@@ -12,9 +14,18 @@ public class PlayerController : MonoBehaviour
     public float attackSpeed = 1;
     public AttackType attackType = AttackType.NONE;
 
+    public GameObject projectiles;
+    private int projectileCounter = 0;
+    public float projectileSpeed = 10f;
+
+    void Start()
+    {
+        Attack();
+
+    }
+
     void Update()
     {
-        
     }
 
     public void FixedUpdate()
@@ -26,6 +37,17 @@ public class PlayerController : MonoBehaviour
 
     private void Attack()
     {
+        Vector3 target = new Vector3(float.MaxValue, float.MaxValue, 0);
+        for (int i=0; i<enemies.transform.childCount;i++)
+        {
+            if (Vector3.Distance(transform.position, enemies.transform.GetChild(i).transform.position)< Vector3.Distance(transform.position, target))
+            {
+                target = enemies.transform.GetChild(i).transform.position;
+            }
+        }
+        GameObject projectile = projectiles.transform.GetChild(projectileCounter).gameObject;
+        projectile.transform.rotation = new Quaternion(0, 0, Vector3.Angle(transform.position, target), 1);
 
+        projectile.SetActive(true);
     }
 }
